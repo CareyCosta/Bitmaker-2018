7044d5452f8fd3d23fd11c1a4f5ba028c8edcabf
Carey Costa <careycosta@Careys-MacBook-Pro.local>
2018-05-28 00:22:49 -0400

    cat.rb exercise complete

--- oop/cats_meow/cats.rb
+++ oop/cats_meow/cats.rb
@@ -16,7 +16,23 @@ class Cat
   def meal_time
     @meal_time
   end
+
+  def eats_at
+    m = meal_time
+    if m < 12
+      puts "#{name} eats at #{m}am"
+    else
+      n = m / 2
+        puts "#{name} eats at #{n}pm"
+    end
+  end
+
+  def meow
+    "My name is #{name}, and I eat #{preferred_food}"
+  end
 end
 
-cat1 = Cat.new('wilbur', 'apples', '12oclock')
-puts cat1.name
+cat1 = Cat.new('wilbur', 'apples', 22)
+puts cat1.eats_at
+cat2 = Cat.new('carey', 'oranges', 6)
+puts cat2.eats_at
