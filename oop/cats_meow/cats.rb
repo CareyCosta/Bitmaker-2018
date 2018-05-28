15552323596fe85a1648bdf101a9aeb1486f49aa
Carey Costa <careycosta@Careys-MacBook-Pro.local>
2018-05-27 23:27:36 -0400

    created instance of Cat called cat1

--- oop/cats_meow/cats.rb
+++ oop/cats_meow/cats.rb
@@ -1,2 +1,22 @@
 class Cat
+  def initialize(n, p, m)
+    @name = n
+    @preferred_food = p
+    @meal_time = m
+  end
+
+  def name
+    @name
+  end
+
+  def preferred_food
+    @preferred_food
+  end
+
+  def meal_time
+    @meal_time
+  end
 end
+
+cat1 = Cat.new('wilbur', 'apples', '12oclock')
+puts cat1.name
