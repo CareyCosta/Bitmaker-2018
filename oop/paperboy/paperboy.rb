348616c28492c7cc79bcf3cb9f2753c9e06dc1ac
Carey Costa <careyxcosta@gmail.com>
2018-05-28 11:25:01 -0400

    earnings and quota methods work

--- oop/paperboy/paperboy.rb
+++ oop/paperboy/paperboy.rb
@@ -1,8 +1,8 @@
 class Paperboy
-  def initialize(name, experience, earnings)
+  def initialize(name)
     @name = name
-    @experience = experience
-    @earnings = earnings
+    @experience = 0
+    @earnings = 0
   end
 
 # reader method, gets name
@@ -10,24 +10,39 @@ class Paperboy
     @name
   end
 
-# writer method, writes name
-  def name=(str)
-    @name = str
-  end
-
 # reader method, gets experience
   def experience
     @experience
-  end
-
-# writer method
-  def experience=(str)
-    @experience = str
   end
 
 # reader method
   def earnings
     @earnings
   end
-  
+
+  def quota
+    min = 50
+    q = (@experience % 2) + min
+    p "your quota is #{q}"
+  end
+
+  def deliver(start_address, end_address)
+    houses = (end_address - start_address)
+      puts "you have #{houses} houses to deiver to"
+    @earnings = houses * 0.25
+    if houses > 50
+      @earnings = (houses - 50) * 0.50
+    end
+    puts "did you complete this deliver?"
+      answer = gets.chomp
+        if answer == "yes"
+          puts "you have earned $#{@earnings}"
+        end
+  end
+
 end
+
+boy1 = Paperboy.new('kevin')
+boy1.quota
+boy1.deliver(20, 50)
+boy1.earnings
