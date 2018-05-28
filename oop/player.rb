b198ff5938ce5e22d8f1c3d7e7a8805de06206b9
Carey Costa <careyxcosta@gmail.com>
2018-05-28 10:25:09 -0400

    confirmed player.rb works :D

--- oop/player.rb
+++ oop/player.rb
@@ -1,24 +1,63 @@
 class Player
 
-def initialize(gold_coins, health_points, lives)
-  @gold_coins = 0
-  @health_points = 10
-  @lives = 5
-end
+  def initialize
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+  end
 
-def level_up
-  @lives += 1
-end
+  def gold_coins
+    return @gold_coins
+  end
 
-def collect_treasure
-  if @gold_coins % 10
-    return @gold_coins == 0
-      @gold_coins += 1
+  def health_points
+    return @health_points
+  end
+
+  def lives
+    return @lives
+  end
+
+  def level_up
+    @lives += 1
+    puts "your lives are at #{@lives}"
+  end
+
+  def collect_treasure
+    @gold_coins += 1
+    puts "your gold coins are at #{@gold_coins}"
+      if (@gold_coins % 10) == 0
+          level_up
+      end
+  end
+
+  def restart
+    @gold_coins = 0
+    @health_points = 10
+    @lives = 5
+    puts @gold_coins
+    puts @health_points
+    puts @lives
+  end
+
+  def do_battle(d)
+    @health_points -= d
+      if @health_points == 1
+        @lives -= 1
+        puts "your lives are at #{@lives}"
+        # @health_points = 10
+      end
+      if @health_points == 0
+        puts "you must restart"
+        restart
+      end
   end
 end
 
-def do_battle(damage)
-  
-end
-
-end
+player1 = Player.new
+# player1.level_up
+# 9.times do
+#   player1.collect_treasure
+# end
+# player1.collect_treasure
+player1.do_battle(10)
